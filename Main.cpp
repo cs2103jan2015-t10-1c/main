8cb05ec0e5348b3683d5e5a89fa11ba0339c4fc2
Christian <christian.h.wijaya@gmail.com>
2015-03-04 18:34:27 +0800

    revert to original

--- Main.cpp
+++ Main.cpp
@@ -3,7 +3,6 @@
 using namespace std;
 
 int main(){
-	//yo look at this
 	DataStructure helloworld;
 	helloworld.createContent("bisma", helloworld.newDate(2, 3, 1980), helloworld.newTime(19, 30), helloworld.newTime(20, 30), "haw par villa");
 	helloworld.createContent("hawe", helloworld.newDate(3, 4, 1995), helloworld.newTime(10, 40), helloworld.newTime(11, 40), "joo koon");
