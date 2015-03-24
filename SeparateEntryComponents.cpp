0e32583bf79b161108a4db839b6c202979314a69
nvmn0601 <nvmn0601@gmail.com>
2015-03-24 23:47:32 +0800

    Refactoring: Removing magic numbers

--- SeparateEntryComponents.cpp
+++ SeparateEntryComponents.cpp
@@ -14,76 +14,89 @@ const string MONTH_OCTOBER = "October";
 const string MONTH_NOVEMBER = "November";
 const string MONTH_DECEMBER = "December";
 
-const string SeparateEntryComponents::STARTDATE_MARKER = "from";
-const string SeparateEntryComponents::LOCATION_MARKER = "at";
-const string SeparateEntryComponents::NAME_MARKER = ".";
-const string SeparateEntryComponents::ENDDATE_MARKER = "on";
-const string SeparateEntryComponents::TAG_MARKER = "#";
 const string SeparateEntryComponents::MONTH_ARRAY[12] = {MONTH_JANUARY, MONTH_FEBRUARY, MONTH_MARCH, MONTH_APRIL, MONTH_MAY, MONTH_JUNE,
 	MONTH_JULY, MONTH_AUGUST, MONTH_SEPTEMBER, MONTH_OCTOBER, MONTH_NOVEMBER, MONTH_DECEMBER};
 
-void SeparateEntryComponents::dissectCommand (string entryComponents, string& entryName, string& entryStartTime, string& entryEndTime,
-							 string& entryStartDate, string& entryEndDate, string& entryLocation, string& tag){
-	size_t pos = 0;
-	extractName(entryComponents, pos, entryName);
+const int SeparateEntryComponents::BLANK_SPACE_COUNT = 1;
+const string SeparateEntryComponents::AT_MARKER = "at";
+const string SeparateEntryComponents::BLANK_SPACE = " ";
+const string SeparateEntryComponents::FROM_MARKER = "from";
+const string SeparateEntryComponents::FULLSTOP_MARKER = ".";
+const string SeparateEntryComponents::ON_MARKER = "on";
+const string SeparateEntryComponents::TAG_MARKER = "#";
+const string SeparateEntryComponents::TO_MARKER = "to";
 
-	if (entryComponents[0] == STARTDATE_MARKER[0] && entryComponents[1] == STARTDATE_MARKER[1]){
-		entryComponents.erase(0, STARTDATE_MARKER.size() + 1);
-		extractDate(entryComponents, pos, entryStartDate);
-		extractTime(entryComponents, pos, entryStartTime);
-		entryComponents = entryComponents.substr(3, entryComponents.size()-1);
-		extractDate(entryComponents, pos, entryEndDate);
-		extractTime(entryComponents, pos, entryEndTime);
+
+void SeparateEntryComponents::dissectCommand (string entryComponents, string& entryName, string& entryStartTime, string& entryEndTime,
+				                              string& entryStartDate, string& entryEndDate, string& entryLocation, string& tag){
+	extractName(entryComponents, entryName);
+
+	if (entryComponents[0] == FROM_MARKER[0] && entryComponents[1] == FROM_MARKER[1]){
+		int startDatePosition = FROM_MARKER.size() + BLANK_SPACE_COUNT;
+		entryComponents.erase(0, startDatePosition);
+		extractDate(entryComponents, entryStartDate);
+		extractTime(entryComponents, entryStartTime);
+		int endDatePosition = TO_MARKER.size() + BLANK_SPACE_COUNT;
+		int endOfLine = entryComponents.size() - 1;
+		entryComponents = entryComponents.substr(endDatePosition, endOfLine);
+		extractDate(entryComponents, entryEndDate);
+		extractTime(entryComponents, entryEndTime);
 	}
+
 	else{
-		extractTime(entryComponents, pos, entryStartTime);
-		extractTime(entryComponents, pos, entryEndTime);
-		if (entryComponents[0] == ENDDATE_MARKER[0] && entryComponents[1] == ENDDATE_MARKER[1]){
-			entryComponents.erase(0,3);
-			extractDate(entryComponents, pos, entryStartDate);
+		extractTime(entryComponents, entryStartTime);
+		extractTime(entryComponents, entryEndTime);
+		if (entryComponents[0] == ON_MARKER[0] && entryComponents[1] == ON_MARKER[1]){
+			int datePosition = ON_MARKER.size() + BLANK_SPACE_COUNT;
+			entryComponents.erase(0, datePosition);
+			extractDate(entryComponents, entryStartDate);
 		}
 	}
-	if (entryComponents[0] == LOCATION_MARKER[0] && entryComponents[1] == LOCATION_MARKER[1]){
-		extractLocation(entryComponents, pos, entryLocation);
+
+	if (entryComponents[0] == AT_MARKER[0] && entryComponents[1] == AT_MARKER[1]){
+		extractLocation(entryComponents, entryLocation);
 	}
 
 	if (entryComponents[0] == TAG_MARKER[0]){
-		extractTag(entryComponents, pos, tag);
+		extractTag(entryComponents, tag);
 	}
 }
 
-void SeparateEntryComponents::extractTag(string& entryComponents, size_t pos, string& entryTag){
-	pos = entryComponents.size();
-	entryTag = entryComponents.substr(0, pos);
-    entryComponents.erase(0, pos);
+void SeparateEntryComponents::extractTag(string& entryComponents, string& entryTag){
+	int endOfTag = entryComponents.size();
+	entryTag = entryComponents.substr(0, endOfTag);
+	entryComponents.erase(0, endOfTag);
 }
 	
-void SeparateEntryComponents::extractName(string& entryComponents, size_t pos, string& entryName){
-	pos = entryComponents.find(NAME_MARKER);
-    entryName = entryComponents.substr(1, pos-1);
-    entryComponents = entryComponents.substr(pos+2, entryComponents.size() - 1);
+void SeparateEntryComponents::extractName(string& entryComponents, string& entryName){
+	int endOfName = entryComponents.find(FULLSTOP_MARKER) - 1;
+	entryName = entryComponents.substr(1, endOfName);
+	cout << entryName << endl;
+	int startOfRemainder = endOfName + FULLSTOP_MARKER.size() + BLANK_SPACE_COUNT + 1;
+	int endOfRemainder = entryComponents.size() - 1;
+	entryComponents = entryComponents.substr(startOfRemainder, endOfRemainder);
+	cout << entryComponents << endl;
 }
 
-void SeparateEntryComponents::extractTime(string& entryComponents, size_t pos, string& entryTime){
-	int index = entryComponents.find(' ');
-    entryTime = entryComponents.substr(0, index);
-    entryComponents.erase(0, index+1);
+void SeparateEntryComponents::extractTime(string& entryComponents, string& entryTime){
+	int endOfTime = entryComponents.find(BLANK_SPACE);
+	entryTime = entryComponents.substr(0, endOfTime);
+    entryComponents.erase(0, endOfTime + BLANK_SPACE_COUNT);
 }
 
-void SeparateEntryComponents::extractDate(string& entryComponents, size_t pos, string& entryDate){
-    int index = entryComponents.find(' ');
-	index = entryComponents.find(' ', index + 1);
-	index = entryComponents.find(' ', index + 1);
-	entryDate = entryComponents.substr(0, index);
-    entryComponents.erase(0, index+1);
+void SeparateEntryComponents::extractDate(string& entryComponents, string& entryDate){
+    int endOfDay = entryComponents.find(BLANK_SPACE);
+	int endOfMonth = entryComponents.find(BLANK_SPACE, endOfDay + BLANK_SPACE_COUNT);
+	int endOfYear = entryComponents.find(BLANK_SPACE, endOfMonth + BLANK_SPACE_COUNT);
+	entryDate = entryComponents.substr(0, endOfYear);
+	entryComponents.erase(0, endOfYear + BLANK_SPACE_COUNT);
 }
 
-
-void SeparateEntryComponents::extractLocation(string& entryComponents, size_t pos, string& entryLocation){
-    int leftBound = entryComponents.find(' ');
-	int rightBound = entryComponents.find(' ', leftBound + 1);
-	entryLocation = entryComponents.substr(leftBound+1, rightBound-leftBound);
-    entryComponents.erase(0, rightBound+1);
+void SeparateEntryComponents::extractLocation(string& entryComponents, string& entryLocation){
+	int startOfLocation = entryComponents.find(BLANK_SPACE) + BLANK_SPACE_COUNT;
+	int endOfLocation = entryComponents.find(BLANK_SPACE, startOfLocation);
+	entryLocation = entryComponents.substr(startOfLocation, endOfLocation - startOfLocation);
+    entryComponents.erase(0, endOfLocation + BLANK_SPACE_COUNT);
 }
 
 void SeparateEntryComponents::convertTime(string entryTime, int& entryHour, int& entryMin){
@@ -92,17 +105,17 @@ void SeparateEntryComponents::convertTime(string entryTime, int& entryHour, int&
 	string end;
 	iss >> start >> end;
 
-	size_t startDot = start.find_first_of(".");
+	size_t startDot = start.find_first_of(FULLSTOP_MARKER);
 	string startHourString = start.substr(0, startDot);
 	istringstream convertStartHour(startHourString);
 	if (!(convertStartHour >> entryHour)) {
-		entryHour = 0;  
+		entryHour = 0;
 	}
 	
-	string startMinString = start.substr(startDot+1, string::npos);
+	string startMinString = start.substr(startDot + 1, string::npos);
 	istringstream convertStartMin(startMinString);
 	if (!(convertStartMin >> entryMin)) {
-		entryMin = 0;  
+		entryMin = 0; 
 	}
 
 	return;
@@ -128,7 +141,7 @@ void SeparateEntryComponents::convertDate(string entryDate, int& day, int& month
 	bool found = false;
 	int monthCount = 1;
 	while (!found){
-		if (monthString == MONTH_ARRAY[monthCount-1]){
+		if (monthString == MONTH_ARRAY[monthCount - 1]){
 			found = true;
 			month = monthCount;
 		}
--- SeparateEntryComponents.h
+++ SeparateEntryComponents.h
@@ -8,20 +8,24 @@ using namespace std;
 
 class SeparateEntryComponents{
 private:
+	
 	static const string MONTH_ARRAY[12];
-	static const string STARTDATE_MARKER;
-	static const string ENDDATE_MARKER;
-	static const string LOCATION_MARKER;
-	static const string NAME_MARKER;
+	static const int BLANK_SPACE_COUNT;
+	static const string AT_MARKER;
+	static const string BLANK_SPACE;
+	static const string FROM_MARKER;
+	static const string FULLSTOP_MARKER;
+	static const string ON_MARKER;
 	static const string TAG_MARKER;
-
+	static const string TO_MARKER;
+	
 public:
 	 void dissectCommand(string, string&, string&, string&, string&, string&, string&, string&);
-	 void extractName(string&, size_t, string&);
-	 void extractTime(string&, size_t, string&);
-	 void extractDate(string&, size_t, string&);
-	 void extractLocation(string&, size_t, string&);
-	 void extractTag(string&, size_t, string&);
+	 void extractName(string&, string&);
+	 void extractTime(string&, string&);
+	 void extractDate(string&, string&);
+	 void extractLocation(string&, string&);
+	 void extractTag(string&, string&);
 	 void convertTime(string, int&, int&);
 	 void convertDate(string, int&, int&, int&);
 };
