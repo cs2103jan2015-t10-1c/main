2c5e4b56c0b876c261daedd1b151bf624c8f20c7
Bisma <ypbisma@gmail.com>
2015-04-05 23:09:23 +0800

    Commit DateTimeInitialiser

---
+++ DateTimeInitialiser.h
@@ -0,0 +1,20 @@
+#ifndef DATETIMEINITIALISER_H
+#define DATETIMEINITIALISER_H
+
+#include "Date.h"
+#include "Time.h"
+#include <boost\date_time\gregorian\gregorian.hpp>
+#include <boost\date_time\posix_time\posix_time.hpp>
+
+using namespace std;
+using namespace boost::gregorian;
+using namespace boost::posix_time;
+
+class DateTimeInitialiser{
+
+public:
+	DateTimeInitialiser();
+	void initialiseDate(Date&, int, int, int);
+	void initialiseTime(Time&, int, int, date);
+};
+#endif
