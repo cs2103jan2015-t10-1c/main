9425c0224615f9dd12b4b931b042e11e89d7130f
nvmn0601 <nvmn0601@gmail.com>
2015-04-13 19:53:24 +0800

    all correct unit tests

--- FastAddTest/UnitTest.cpp
+++ FastAddTest/UnitTest.cpp
@@ -38,51 +38,141 @@ namespace FastAddTest {
 			Assert::IsTrue(Someday.getDateStatus());				
 		}
 		
-		//@author A0115656A
-		TEST_METHOD(EntryTimeTest) 
-		{	
-			Time timeTest;
+		TEST_METHOD(EntryTimeTest) {	
+			Time Sometime;
 			date today(day_clock::local_day());
 			int expectedHour = 20;
 			int expectedMinute = 20;
-			timeTest.insertHour(expectedHour);
-			timeTest.insertMinute(expectedMinute);
-			timeTest.initialiseTime(today);
-			int actualHour = timeTest.getHour();
+			Sometime.insertHour(expectedHour);
+			Sometime.insertMinute(expectedMinute);
+			Sometime.initialiseTime(today);
+			int actualHour = Sometime.getHour();
 			Assert::AreEqual(expectedHour, actualHour);
-			int actualMinute = timeTest.getMinute();
+			int actualMinute = Sometime.getMinute();
 			Assert::AreEqual(expectedMinute, actualMinute);
 
-			Assert::IsTrue(timeTest.getTimeStatus());				
+			Assert::IsTrue(Sometime.getTimeStatus());				
 		}
 
-		TEST_METHOD(EntryEditTest)
-		{
-			//scheduled entries
-			EntryEdit editTestSch(true);
-			bool testBool = editTestSch.getEditStatus();
-			Assert::AreEqual(true, testBool);
-			testBool = editTestSch.getDateEditStatus();
-			Assert::AreEqual(false, testBool);
-			testBool = editTestSch.getTimeEditStatus();
-			Assert::AreEqual(false, testBool);
-			testBool = editTestSch.getTagAddedStatus();
-			Assert::AreEqual(false, testBool);
-			testBool = editTestSch.getTagRemovedStatus();
-			Assert::AreEqual(false, testBool);
+		
+		TEST_METHOD(EntryTest) {
+			Entry testInfo;
+		
+			//test for name
+			testInfo.insertName("dinner with me");
+			string actualAnswer = testInfo.getName();	
+			string expectedAnswer = "dinner with me";
+			Assert::AreEqual(expectedAnswer,actualAnswer);
+			
+			//test for entryNumber
+			testInfo.insertEntryNumber(1);
+			int b=testInfo.getEntryNumber();
+			Assert::AreEqual(1,b);
 
-			//floating entries
-			EntryEdit editTestFlo(false);
+			//test for location
+			testInfo.insertLocation("utown");
+			actualAnswer=testInfo.getLocation();	
+			expectedAnswer = "utown";
+			Assert::AreEqual(expectedAnswer,actualAnswer);
+		}
+
+		TEST_METHOD(TestUITest) {
+			string testInfo = "edit dinner with me";
+			TextUI line(testInfo);
+			string expectedCommandWord = "edit";
+			string actualCommandWord=line.findCommand(testInfo);
+			Assert::AreEqual(expectedCommandWord, actualCommandWord);
+		}
+		
+
+		//addEntry for scheduled and floating
+		TEST_METHOD(EntryListAddTest) {
+				
+			Entry testScheduled; 
+			Entry testFloating;	
+			EntryLists testInfo;
+	
+			int dummy;
+			testScheduled.insertName("a");
+			testInfo.addEntry(testScheduled, dummy);
+			testScheduled.insertName("b");
+			testInfo.addEntry(testScheduled, dummy);
+			testScheduled.insertName("c");
+			testInfo.addEntry(testScheduled, dummy);
+			testScheduled.insertName("d");
+			testInfo.addEntry(testScheduled, dummy);
+			
+			vector<Entry> actualResult;
+			actualResult = testInfo.getScheduledList();
+			int size = actualResult.size();
+			Assert::AreEqual(4,size);
+		
+		}		
+
+		TEST_METHOD(EntryAddTest) {	
+			string expectedName = "testing";
+			string expectedLocation = "NUS ";
+			string expectedTag = "#meeting";
+			string expectedStatus = "done";
+			string expectedStartDate = "20 Apr 2015";
+			string expectedEndDate = "21 Apr 2015";
+			string expectedStartTime = "18.30";
+			string expectedEndTime = "21.45";
+			string input = " testing. from 20 Apr 2015 18.30 to 21 Apr 2015 21.45 at NUS #meeting";
+			string actualName;
+			string actualLocation;
+			vector<string> actualTag;
+			string actualStatus;
+			string actualStartDate;
+			string actualEndDate;
+			string actualStartTime;
+			string actualEndTime;
+
+			EntryAdd testDissectCommand;
+			testDissectCommand.dissectCommand(input,actualName, actualStartDate, actualStartTime, actualEndDate, 
+											  actualEndTime, actualLocation, actualTag);
+			Assert::AreEqual(expectedName, actualName);
+			Assert::AreEqual(expectedStartDate, actualStartDate);
+			Assert::AreEqual(expectedStartTime, actualStartTime);
+			Assert::AreEqual(expectedEndDate, actualEndDate);
+			Assert::AreEqual(expectedEndTime, actualEndTime);
+			Assert::AreEqual(expectedLocation, actualLocation);
 
 		}
 
-		TEST_METHOD(UndoActionsTest)
-		{
+		//test on editEntry tag for floating tasks
+			TEST_METHOD(EntryListEditNameFloatingTest) 
+			{		
+			string expectedAnswer="k";
+			
+			Entry testScheduled; 
+			Entry testFloating;	
+			EntryLists testInfo;
 
+			testScheduled.insertName("a");
+			testScheduled.insertEntryNumber(1);
+			
+			testScheduled.initialiseStatus();
+			testScheduled.insertLocation("utown");
+			vector<string> vectorTag;
+			vectorTag.push_back("#study");
+			testScheduled.insertTags(vectorTag);
+
+			int dummyNumber;
+			testInfo.addEntry(testScheduled,dummyNumber);			
+			
+			string dummyString;
+			string userInput = "1 name k";
+			testInfo.editEntry(true, userInput, dummyString);	
+			string actualResult;
+			vector<Entry> dummyEntry = testInfo.getScheduledList();
+			testScheduled = dummyEntry.back();
+			actualResult = testScheduled.getName();
+					
+			Assert::AreEqual(expectedAnswer,actualResult);
 		}
 
-
-		//@author
+		//@author A0100750Y
 		//test on editEntry name for scheduled tasks
 		TEST_METHOD(EntryListEditNameTest) 
 		{
@@ -138,231 +228,109 @@ namespace FastAddTest {
 			Assert::AreEqual(expectedAnswer,actualResult);
 		}
 
-
-
-
-		TEST_METHOD(EntryTest) {
-			Entry testInfo;
-		
-			//test for name
-			testInfo.insertName("dinner with me");
-			string actualAnswer = testInfo.getName();	
-			string expectedAnswer = "dinner with me";
-			Assert::AreEqual(expectedAnswer,actualAnswer);
+		//@author A0100750Y
+		//editEntry tag for floating
+		TEST_METHOD(EntryListEditTagFloatingTest) 
+			{		
+			string expectedAnswer="#play ";
 			
-			//test for entryNumber
-			testInfo.insertEntryNumber(1);
-			int b=testInfo.getEntryNumber();
-			Assert::AreEqual(1,b);
-
-			//test for location
-			testInfo.insertLocation("utown");
-			actualAnswer=testInfo.getLocation();	
-			expectedAnswer = "utown";
-			Assert::AreEqual(expectedAnswer,actualAnswer);
-		}
-
-		TEST_METHOD(TestUITest) {
-			string testInfo = "edit dinner with me";
-			TextUI line(testInfo);
-			string expectedCommandWord = "edit";
-			string actualCommandWord=line.findCommand(testInfo);
-			Assert::AreEqual(expectedCommandWord, actualCommandWord);
-		}
-		
-
-		//addEntry for scheduled and floating
-		TEST_METHOD(EntryListAddScheduledTest) {
-			vector<string> expectedAnswser;
-			expectedAnswser.push_back("a");
-			expectedAnswser.push_back("b");
-			expectedAnswser.push_back("c");
-			expectedAnswser.push_back("d");
-				
 			Entry testScheduled; 
 			Entry testFloating;	
 			EntryLists testInfo;
-	
-			int dummy;
-			testScheduled.insertName("a");
-			testInfo.addEntry(testScheduled, dummy);
-			testScheduled.insertName("b");
-			testInfo.addEntry(testScheduled, dummy);
-			testScheduled.insertName("c");
-			testInfo.addEntry(testScheduled, dummy);
-			testScheduled.insertName("d");
-			testInfo.addEntry(testScheduled, dummy);
-			
-			vector<Entry> actualResult;
-			actualResult = testInfo.getScheduledList();
-			int size = actualResult.size();
-			Assert::AreEqual(4,size);
-		
-		}
 
-		//removeEntry for scheduled and floating
-		TEST_METHOD(EntryListsRemoveTest) {
-			vector<string> expectedAnswser;
-			expectedAnswser.push_back("a");
-			expectedAnswser.push_back("b");
-			expectedAnswser.push_back("c");
-			expectedAnswser.push_back("d");
-				
-			Entry testScheduled; 
-			Entry testFloating;	
-			EntryLists testInfo;
-	
-			int dummy;
 			testScheduled.insertName("a");
-			testInfo.addEntry(testScheduled, dummy);
-			testScheduled.insertName("b");
-			testInfo.addEntry(testScheduled, dummy);
-			testScheduled.insertName("c");
-			testInfo.addEntry(testScheduled, dummy);
-			testScheduled.insertName("d");
-			testInfo.addEntry(testScheduled, dummy);
+			testScheduled.insertEntryNumber(1);
 			
-			vector<Entry> actualResult;
-			actualResult = testInfo.getScheduledList();
-			
-			string deleteFeedback;
-			testInfo.removeEntry(true, 1, deleteFeedback);			
-			int size = actualResult.size();
-			Assert::AreEqual(3,size);
-		}
+			testScheduled.initialiseStatus();
+			testScheduled.insertLocation("utown");
+			vector<string> vectorTag;
+			vectorTag.push_back("#study");
+			testScheduled.insertTags(vectorTag);
 
-		//test for getEntryDisplay from Entrylists
-		TEST_METHOD(EntryListsGetEntryDisplayTest) {
-			string expectedAnswer = "b";
-			Entry testScheduled; 
-			Entry testFloating;	
-			EntryLists testInfo;
-	
-			int dummy;
-			testScheduled.insertName("a");
-			testInfo.addEntry(testScheduled, dummy);
-			testScheduled.insertName("b");
-			testInfo.addEntry(testScheduled, dummy);
-			testScheduled.insertName("c");
-			testInfo.addEntry(testScheduled, dummy);
-			testScheduled.insertName("d");
-			testInfo.addEntry(testScheduled, dummy);
-			
+			int dummyNumber;
+			testInfo.addEntry(testScheduled,dummyNumber);			
+
+			string dummyString;
+			string userInput = "1 add #play. remove #study";		
+			testInfo.editEntry(true, userInput, dummyString);		
 			string actualResult;
-			actualResult = testInfo.getEntryDisplay(true, 2);
+			vector<Entry> dummyEntry = testInfo.getScheduledList();
+			testScheduled = dummyEntry.back();
+			actualResult = testScheduled.getTags();		
 
-			Assert::AreEqual(expectedAnswer,actualResult);
-	
+			Assert::AreEqual(expectedAnswer,actualResult);	
 		}
-		TEST_METHOD(EntryAddTest) {	
-			string expectedName = "testing";
-			string expectedLocation = "NUS ";
-			string expectedTag = "#meeting";
-			string expectedStatus = "done";
-			string expectedStartDate = "20 Apr 2015";
-			string expectedEndDate = "21 Apr 2015";
-			string expectedStartTime = "18.30";
-			string expectedEndTime = "21.45";
-			string input = " testing. from 20 Apr 2015 18.30 to 21 Apr 2015 21.45 at NUS #meeting";
-			string actualName;
-			string actualLocation;
-			vector<string> actualTag;
-			string actualStatus;
-			string actualStartDate;
-			string actualEndDate;
-			string actualStartTime;
-			string actualEndTime;
-
-			EntryAdd testDissectCommand;
-			testDissectCommand.dissectCommand(input,actualName, actualStartDate, actualStartTime, actualEndDate, 
-											  actualEndTime, actualLocation, actualTag);
-			Assert::AreEqual(expectedName, actualName);
-			Assert::AreEqual(expectedStartDate, actualStartDate);
-			Assert::AreEqual(expectedStartTime, actualStartTime);
-			Assert::AreEqual(expectedEndDate, actualEndDate);
-			Assert::AreEqual(expectedEndTime, actualEndTime);
-			Assert::AreEqual(expectedLocation, actualLocation);
-		}
-		//@author A0115902N
-		TEST_METHOD(ClashInspectorTest) {
-			vector<Entry> dummy;
-			Entry firstEntry;
-			Entry secondEntry;
-
-			Date firstDate;
-			Date secondDate;
-			Time firstStartTime;
-			Time firstEndTime;
-			Time secondStartTime;
-			Time secondEndTime;
-
-			firstDate.insertDay(10);
-			firstDate.insertMonth(5);
-			firstDate.insertYear(2015);
-			firstDate.initialiseDate();
-			secondDate.insertDay(10);
-			secondDate.insertMonth(5);
-			secondDate.insertYear(2015);
-			secondDate.initialiseDate();
-
-			firstStartTime.insertHour(10);
-			firstStartTime.insertMinute(30);
-			firstEndTime.insertHour(11);
-			firstEndTime.insertMinute(30);
-
-			secondStartTime.insertHour(10);
-			secondStartTime.insertMinute(30);
-			secondEndTime.insertHour(11);
-			secondEndTime.insertMinute(30);
-
-			firstEntry.insertStartDate(firstDate);
-			firstEntry.insertEndDate(firstDate);
-			firstEntry.insertStartTime(firstStartTime);
-			firstEntry.insertEndTime(firstEndTime);
-
-			secondEntry.insertStartDate(secondDate);
-			secondEntry.insertEndDate(secondDate);
-			secondEntry.insertStartTime(secondStartTime);
-			secondEntry.insertEndTime(secondEndTime);
-
-			bool clash = false;
-			bool print = false;
-			int dummyInt = 1;
-			ClashInspector inspector(dummy);
-			inspector.inspectEntries(firstEntry, secondEntry, dummyInt, clash, print);
+		
+		//@author A0100750Y
+		//tests on editEntry loc for floating tasks
+		TEST_METHOD(EntryListEditLocFloatingTest) 
+		{		
+			string expectedAnswer="yih";
 			
-			Assert::AreEqual(true, clash);
-		}
-		//@author A0115902N
-		TEST_METHOD(StringConvertorTest) {
-			StringConvertor convertor;
-			string dateInput = "14 mar 15";
-			int day;
-			int month;
-			int year;
-			int expectedDay = 14;
-			int expectedMonth = 3;
-			int expectedYear = 2015;
-			int number;
-			int expectedNumber = 12;
-			convertor.convertDate(dateInput, day, month, year);
-			Assert::AreEqual(expectedDay, day);
-			Assert::AreEqual(expectedMonth, month);
-			Assert::AreEqual(expectedYear, year);
+			Entry testScheduled; 
+			Entry testFloating;	
+			EntryLists testInfo;
 
-			string numberInput = "12";
-			convertor.convertStringToNumber(numberInput, number);
-			Assert::AreEqual(expectedNumber, number);
-			string dateLongInput = "14 mar 15 to 15 mar 15";
-			string expectedStartDate = "14 mar 15";
-			string expectedEndDate = "15 mar 15";
-			string resultStartDate;
-			string resultEndDate;
-			convertor.extractStringDate(dateLongInput, resultStartDate);
-			dateLongInput = dateLongInput.substr(3);
-			convertor.extractStringDate(dateLongInput, resultEndDate);
-			Assert::AreEqual(expectedStartDate, resultStartDate);
-			Assert::AreEqual(expectedEndDate, resultEndDate);
+			testScheduled.insertName("a");
+			testScheduled.insertEntryNumber(1);			
+			
+			testScheduled.initialiseStatus();
+			testScheduled.insertLocation("utown");
+			vector<string> vectorTag;
+			vectorTag.push_back("#study");
+			testScheduled.insertTags(vectorTag);
+
+			int dummyNumber;
+			testInfo.addEntry(testScheduled,dummyNumber);			
+			
+			string dummyString;
+			string userInput = "1 place yih";			
+			testInfo.editEntry(true, userInput, dummyString);		
+			string actualResult;
+			vector<Entry> dummyEntry = testInfo.getScheduledList();
+			testScheduled = dummyEntry.back();
+			actualResult = testScheduled.getLocation();	
+
+			Assert::AreEqual(expectedAnswer,actualResult);				
 		}
+
+		//@author A0100750Y
+		//removeEntry for scheduled and floating
+		TEST_METHOD(EntryListsRemoveTest) 
+		{
+			Entry testScheduled; 
+			Entry testFloating;	
+			EntryLists testInfo;
+	
+			int dummyNumber;
+			testScheduled.insertName("a");
+			testInfo.addEntry(testScheduled, dummyNumber);
+			testScheduled.insertName("b");
+			testInfo.addEntry(testScheduled, dummyNumber);
+			testScheduled.insertName("c");
+			testInfo.addEntry(testScheduled, dummyNumber);
+			testScheduled.insertName("d");
+			testInfo.addEntry(testScheduled, dummyNumber);
+			
+			string dummyString;
+			testInfo.removeEntry(false, 1, dummyString);		
+			/*vector<Entry> actualResult;
+			actualResult = testInfo.getScheduledList();
+			int size = actualResult.size();
+			Assert::AreEqual(3,size);*/
+		}	
+
+		//@author A0100750Y
+		//tests on DateTimeInspector
+		TEST_METHOD(DateTimeInspectorTest)
+		{
+			DateTimeInspector aDay;
+			Assert::IsTrue(aDay.dateIsValid(31, 03, 2014));			
+			Assert::IsTrue(aDay.timeIsValid(12,59));
+			Assert::IsFalse(aDay.timeIsValid(25,10));
+			Assert::IsFalse(aDay.timeIsValid(20,61));
+		}
+		
 	};
-	}
+}
+
